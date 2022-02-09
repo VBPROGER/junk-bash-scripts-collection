fb8bee05eadbdb38dbf36302473d3d5a82c6868a
VB PROGER <97066898+VBPROGER@users.noreply.github.com>
2022-02-09 23:56:08 +0300

    Create test_tmp.sh

---
+++ scripts/test_tmp.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo -echo :andpass $NONE$ this output isn\'t giving anything :\) &> /dev/null
+echo you can type here whatever you want. if you see an error, try using the "\\" character \(not double\) &> /dev/null
