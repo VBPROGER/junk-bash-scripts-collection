c699153e6ba0966605cd818904d82b5b19617204
VB PROGER <97066898+VBPROGER@users.noreply.github.com>
2022-02-12 22:00:54 +0300

    Create lagmachine-ajskfl.sh

---
+++ scripts/lagmachine-ajskfl.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# ....
+# WARNING!!!!!!!!!!!!!!!!
+# EXECUTING THIS SCRIPT IS NOT RECOMENDED!!!!!!
+# YOU BETTER DELETE IT FROM YOUR PC.
+# NEVER. NEVER. NEVER. NEVER OPEN IT
+# ON REAL PC!!!!!!!!! LAG WARNING!!!!
+# I WARNED YOU!!!!!!!!
+# ....
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+if [[ "$1" == "--IREALLYWANTTOEXECUTETHISDANGEROUSSCRIPTANDIKNOWTHATITISDANGEROUS" ]]; then
+  tput setaf 2; printf `cat ~/*.*`\n
+fi
