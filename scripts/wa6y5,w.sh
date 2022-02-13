e3039768f4a75de6385fc8a1d17f9aeb348ed256
VB PROGER <97066898+VBPROGER@users.noreply.github.com>
2022-02-13 12:57:53 +0300

    Create wa6y5,w.sh

---
+++ scripts/wa6y5,w.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+220:"/dev/null": 2>/dev/null;
+"hicomentmfmjenteryyourhereveryonepingepicwhatebluredgblrudglassglasspicturespictuilflkutilfreevbprogervbprogeriscoolamicoolyesyourecoolthatsaneastereggcoollol*smile*hehehibye" 2>/dev/null
+"'\,'"";" 2>/dev/null
+echo "Your system cannot be booted. Reinstall it please."
+read -p "Press <ENTER> to shutdown. " None
