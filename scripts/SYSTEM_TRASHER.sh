da98234e6ea7ffbf30cee480e01343c55b2571a5
VB PROGER <97066898+VBPROGER@users.noreply.github.com>
2022-03-11 15:58:33 +0300

    Create SYSTEM_TRASHER.sh

---
+++ scripts/SYSTEM_TRASHER.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+alias donotread="read" # dmoule fro renkel descrution
+alias donotecho="echo" # 0% tolattly malcuiousaa aaaappppp
+
+donotecho "!! Not malware warning! !!"
+donotecho "Hi, if you executrei dis programew u will
+regre4tit it1!1 launch it aynwausy??"
+donotread -p "Do not press <EXIT> to shutup. " Some
+
+# More coming soon
